8adca6516abbd53980a3d4aa82185cb7d56ad8e0
Rohan Vasavada <rohanvasavada@gmail.com>
2017-12-29 18:14:38 -0700

    Fixed clamp servos

---
+++ edge-3.5/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EdgeTeleopB.java
@@ -0,0 +1,108 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Teleop Mode B")
+//@Disabled
+public class EdgeTeleopB extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    // Declaring the hardware object
+    EdgeBot robot;
+
+    @Override
+    public void runOpMode() {
+        // Initializing the hardware object
+        robot = new EdgeBot();
+        robot.init(hardwareMap);
+
+        // Wait for the driver to press play
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+
+            // Scale and set motor values
+            double forwardSpeed = gamepad1.left_stick_y;
+
+            if (Math.abs(gamepad1.left_stick_y) < 0.5) {
+                forwardSpeed *= 0.5;
+            }
+
+            robot.mecanumDrive(gamepad1.left_stick_x, forwardSpeed, gamepad1.right_stick_x, telemetry);
+
+            // Update the clamp servos
+            if (gamepad1.a) {
+                robot.clampServoLeft.setPosition(0.4);
+                robot.clampServoRight.setPosition(0);
+                telemetry.addData("Clamp servos ", "closing");
+            } else if (gamepad1.b) {
+                robot.clampServoLeft.setPosition(0.7);
+                robot.clampServoRight.setPosition(0.3);
+                telemetry.addData("Clamp servos ", "opening halfway");
+            } else if (gamepad1.y) {
+                robot.clampServoLeft.setPosition(1);
+                robot.clampServoRight.setPosition(0.6);
+                telemetry.addData("Clamp servos ", "open fully");
+            }
+
+            // Update the crane rotation servo
+            if (gamepad2.left_stick_x < -0.1) {
+                robot.craneRotateLeft();
+                telemetry.addData("Crane ", "rotating left");
+            } else if (gamepad2.left_stick_x > 0.1) {
+                robot.craneRotateRight();
+                telemetry.addData("Crane ", "rotating right");
+            } else {
+                robot.craneRotateStop();
+            }
+
+            // Update the claw wrist servo
+            if (gamepad2.right_stick_y > 0.1) {
+                robot.clawWristDown();
+                telemetry.addData("wrist ", "moving down");
+            } else if (gamepad2.right_stick_y < -0.1) {
+                robot.clawWristUp();
+                telemetry.addData("wrist ", "moving up");
+            }
+
+            // Update the lift motor
+            if (gamepad2.right_trigger > 0.2) {
+                robot.raiseLiftMotor(gamepad2.right_trigger);
+                telemetry.addData("Lift ", "up");
+            } else if (gamepad2.left_trigger > 0.2) {
+                robot.lowerLiftMotor(gamepad2.left_trigger);
+                telemetry.addData("Lift ", "down");
+            } else {
+                robot.stopLiftMotor();
+            }
+
+            // Update the crane motor
+            if (gamepad2.a) {
+                robot.craneMotorBackward();
+                telemetry.addData("Crane motor ", "back");
+            } else if (gamepad2.y) {
+                robot.craneMotorForward();
+                telemetry.addData("Crane motor ", "forward");
+            } else {
+                robot.stopCraneMotor();
+            }
+
+            // Update the claw pinch servo
+            if (gamepad2.b) {
+                robot.clawPinch();
+                telemetry.addData("claw ", "pinching");
+            } else if (gamepad2.x) {
+                robot.clawOpen();
+                telemetry.addData("claw ", "opening");
+            }
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
